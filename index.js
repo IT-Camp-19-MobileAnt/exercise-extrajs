da95443196b428ba90620e05f9ad0be9b7869e3e
fewkamon <88530787+fewkamon@users.noreply.github.com>
2023-06-01 15:46:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // โจทย์พิเศษ 
 
-// ให้น้อง ทำเขียนฟังชั่นกรองข้อมูลจาก array ตัวแปรชื่อ data โดยใช้ async function ในการทำตัวโค้ด
+// ให้น้อง ทำเขียน ฟังชั่นกรองข้อมูลจาก array ตัวแปรชื่อ data โดยกำหนดให้ใช้ async function ด้วยในการทำตัวโค้ด
 // ให้บอกจำนวนหนังสือ ทั้งหมด ที่อยู่ใน categories และ genres
 // และให้ปริ้นข้อมูล ทั้งหมด ที่อยู๋ใน catregsories นั่น
+// เช่น example ถ้า น้อง result("Fiction", "Fantasy") ให้แสดง จำนวน หนังสือทั้งหมด เช่น Books: 20 และ ให้ปริ้นตัว object หลังจากที่น้อง ได้ทำการ แสดงตัว books
 const categories = ['Fiction', 'Non-fiction', 'Sci-fi', 'Mystery', 'Thriller', 'Romance'];
 const genres = ['Fantasy', 'Historical', 'Science Fiction', 'Horror', 'Biography'];
 
@@ -209,7 +210,7 @@ const data = [
     }
 ]
 
-async function result() {
+async function result(category, genre) {
     //เขียนโค้ดในนี้
 }
 
