d78ca5bba5f46bf82272032425e3adff890da2fc
fewkamon <88530787+fewkamon@users.noreply.github.com>
2023-06-01 15:41:31 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,217 @@
+// โจทย์พิเศษ 
+
+// ให้น้อง ทำเขียนฟังชั่นกรองข้อมูลจาก array ตัวแปรชื่อ data โดยใช้ async function ในการทำตัวโค้ด
+// ให้บอกจำนวนหนังสือ ทั้งหมด ที่อยู่ใน categories และ genres
+// และให้ปริ้นข้อมูล ทั้งหมด ที่อยู๋ใน catregsories นั่น
+const categories = ['Fiction', 'Non-fiction', 'Sci-fi', 'Mystery', 'Thriller', 'Romance'];
+const genres = ['Fantasy', 'Historical', 'Science Fiction', 'Horror', 'Biography'];
+
+const data = [
+    {
+        title: 'Book 1',
+        author: 'Author 1',
+        publicationYear: 2023,
+        ISBN: '9781234567891',
+        publisher: 'Publisher 1',
+        genre: 'Biography',
+        pages: 251,
+        category: 'Thriller'
+    },
+    {
+        title: 'Book 2',
+        author: 'Author 2',
+        publicationYear: 2023,
+        ISBN: '9781234567892',
+        publisher: 'Publisher 2',
+        genre: 'Horror',
+        pages: 252,
+        category: 'Thriller'
+    },
+    {
+        title: 'Book 3',
+        author: 'Author 3',
+        publicationYear: 2023,
+        ISBN: '9781234567893',
+        publisher: 'Publisher 3',
+        genre: 'Science Fiction',
+        pages: 253,
+        category: 'Thriller'
+    },
+    {
+        title: 'Book 4',
+        author: 'Author 4',
+        publicationYear: 2023,
+        ISBN: '9781234567894',
+        publisher: 'Publisher 4',
+        genre: 'Fantasy',
+        pages: 254,
+        category: 'Mystery'
+    },
+    {
+        title: 'Book 5',
+        author: 'Author 5',
+        publicationYear: 2023,
+        ISBN: '9781234567895',
+        publisher: 'Publisher 5',
+        genre: 'Science Fiction',
+        pages: 255,
+        category: 'Romance'
+    },
+    {
+        title: 'Book 6',
+        author: 'Author 6',
+        publicationYear: 2023,
+        ISBN: '9781234567896',
+        publisher: 'Publisher 6',
+        genre: 'Historical',
+        pages: 256,
+        category: 'Non-fiction'
+    },
+    {
+        title: 'Book 7',
+        author: 'Author 7',
+        publicationYear: 2023,
+        ISBN: '9781234567897',
+        publisher: 'Publisher 7',
+        genre: 'Fantasy',
+        pages: 257,
+        category: 'Fiction'
+    },
+    {
+        title: 'Book 8',
+        author: 'Author 8',
+        publicationYear: 2023,
+        ISBN: '9781234567898',
+        publisher: 'Publisher 8',
+        genre: 'Biography',
+        pages: 258,
+        category: 'Fiction'
+    },
+    {
+        title: 'Book 9',
+        author: 'Author 9',
+        publicationYear: 2023,
+        ISBN: '9781234567899',
+        publisher: 'Publisher 9',
+        genre: 'Horror',
+        pages: 259,
+        category: 'Non-fiction'
+    },
+    {
+        title: 'Book 10',
+        author: 'Author 10',
+        publicationYear: 2023,
+        ISBN: '97812345678910',
+        publisher: 'Publisher 10',
+        genre: 'Historical',
+        pages: 260,
+        category: 'Fiction'
+    },
+    {
+        title: 'Book 11',
+        author: 'Author 11',
+        publicationYear: 2023,
+        ISBN: '97812345678911',
+        publisher: 'Publisher 11',
+        genre: 'Science Fiction',
+        pages: 261,
+        category: 'Romance'
+    },
+    {
+        title: 'Book 12',
+        author: 'Author 12',
+        publicationYear: 2023,
+        ISBN: '97812345678912',
+        publisher: 'Publisher 12',
+        genre: 'Horror',
+        pages: 262,
+        category: 'Romance'
+    },
+    {
+        title: 'Book 13',
+        author: 'Author 13',
+        publicationYear: 2023,
+        ISBN: '97812345678913',
+        publisher: 'Publisher 13',
+        genre: 'Fantasy',
+        pages: 263,
+        category: 'Mystery'
+    },
+    {
+        title: 'Book 14',
+        author: 'Author 14',
+        publicationYear: 2023,
+        ISBN: '97812345678914',
+        publisher: 'Publisher 14',
+        genre: 'Historical',
+        pages: 264,
+        category: 'Fiction'
+    },
+    {
+        title: 'Book 15',
+        author: 'Author 15',
+        publicationYear: 2023,
+        ISBN: '97812345678915',
+        publisher: 'Publisher 15',
+        genre: 'Science Fiction',
+        pages: 265,
+        category: 'Thriller'
+    },
+    {
+        title: 'Book 16',
+        author: 'Author 16',
+        publicationYear: 2023,
+        ISBN: '97812345678916',
+        publisher: 'Publisher 16',
+        genre: 'Biography',
+        pages: 266,
+        category: 'Romance'
+    },
+    {
+        title: 'Book 17',
+        author: 'Author 17',
+        publicationYear: 2023,
+        ISBN: '97812345678917',
+        publisher: 'Publisher 17',
+        genre: 'Fantasy',
+        pages: 267,
+        category: 'Mystery'
+    },
+    {
+        title: 'Book 18',
+        author: 'Author 18',
+        publicationYear: 2023,
+        ISBN: '97812345678918',
+        publisher: 'Publisher 18',
+        genre: 'Biography',
+        pages: 268,
+        category: 'Fiction'
+    },
+    {
+        title: 'Book 19',
+        author: 'Author 19',
+        publicationYear: 2023,
+        ISBN: '97812345678919',
+        publisher: 'Publisher 19',
+        genre: 'Horror',
+        pages: 269,
+        category: 'Romance'
+    },
+    {
+        title: 'Book 20',
+        author: 'Author 20',
+        publicationYear: 2023,
+        ISBN: '97812345678920',
+        publisher: 'Publisher 20',
+        genre: 'Fantasy',
+        pages: 270,
+        category: 'Non-fiction'
+    }
+]
+
+async function result() {
+    //เขียนโค้ดในนี้
+}
+
+
+result()
